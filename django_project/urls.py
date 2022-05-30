dd61c74d262754fe40e54bab6e41205513695e0f
Stamatis Spyriounis <stamatis.spyriounis@tripledotstudios.com>
2022-05-30 23:38:23 +0100

    add bootstrap

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
